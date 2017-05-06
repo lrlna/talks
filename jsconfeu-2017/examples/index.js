e4ca1a8b84b80478df8d3df2d81f2b1518a9eb95
lrlna <shestak.irina@gmail.com>
2017-05-06 13:46:12 +0200

    add jsconf eu to readme

--- jsconfeu-2017/examples/index.js
+++ jsconfeu-2017/examples/index.js
@@ -1,11 +1,25 @@
 var http = require('http')
+var zlib = require('zlib')
+var pump = require('pump')
 var fs = require('fs')
 
 http.createServer(compress).listen(8080)
 
 function compress (req, res) {
-  var accept = req.headers['Accept-Encoding']
+  var accept = req.headers['accept-encoding']
   if (!accept) accept = ''
+  var source = fs.createReadStream('index.html')
+
+  if (accept.match(/\bdeflate\b/)) {
+    res.writeHead(200, 'whoa it dflt', { 'Content-Encoding': 'deflate' })
+    pump(source, zlib.createDeflate(), res, handler)
+  } else if (accept.match(/\bgzip\b/)) {
+    res.writeHead(200, 'whooaaa it gzip', { 'Content-Encoding': 'gzip' })
+    pump(source, zlib.createGzip(), res, handler)
+  } else {
+    res.writeHead(200, {})
+    pump(source, res)
+  }
 
   function handler (err) {
     if (err) console.log('errrrr pipe finished with', err)
---
+++ jsdays-it-2017/examples/index.js
@@ -0,0 +1,31 @@
+var brotli = require('iltorb')
+var zlib = require('zlib')
+var pump = require('pump')
+var http = require('http')
+var fs = require('fs')
+
+http.createServer(compress).listen(8080)
+
+function compress (req, res) {
+  var accept = req.headers['accept-encoding']
+  if (!accept) accept = ''
+  var source = fs.createReadStream('index.html')
+
+  if (accept.match(/\bdeflate\b/)) {
+    res.writeHead(200, 'whoa it dflt', { 'Content-Encoding': 'deflate' })
+    pump(source, zlib.createDeflate(), res, handler)
+  } else if (accept.match(/\bgzip\b/)) {
+    res.writeHead(200, 'whooaaa it gzip', { 'Content-Encoding': 'gzip' })
+    pump(source, zlib.createGzip(), res, handler)
+  } else if (accept.match(/\bbr\b/)) {
+    res.writeHead(200, 'whooaaa it broccoli', { 'Content-Encoding': 'br' })
+    pump(source, brotli.compressStream(), res, handler)
+  } else {
+    res.writeHead(200, 'errything is good', {})
+    pump(source, res)
+  }
+
+  function handler (err) {
+    if (err) console.log('errrrr pipe finished with', err)
+  }
+}
